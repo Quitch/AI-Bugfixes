9414d0a2f6de06e0d118c95a5ca0fc58b7db8bb6
Quitch <Quitch@users.noreply.github.com>
2021-04-28 11:11:20 +0100

    Remove unnecessary function

--- ui/mods/com.pa.quitch.AIBugfixEnhancement/difficulty.js
+++ ui/mods/com.pa.quitch.AIBugfixEnhancement/difficulty.js
@@ -5,70 +5,58 @@ if (!aiBugfixLoaded) {
 
   function aiBugfixPersonalities() {
     try {
-      var addToObjectAtIndex = function (obj, key, value, index) {
-        var temp = {};
-        var i = 0;
-
-        for (var prop in obj) {
-          if (Object.prototype.hasOwnProperty.call(obj, prop)) {
-            if (i === index && key && value) {
-              temp[key] = value;
-            }
-
-            temp[prop] = obj[prop];
-
-            i++;
-          }
-        }
-
-        if (!index && key && value) {
-          temp[key] = value;
-        }
-
-        return temp;
+      var temp = {};
+      var i = 0;
+      var newDifficulty = {
+        display_name: loc("!LOC:Normal") + "+",
+        percent_vehicle: 0.45,
+        percent_bot: 0.25,
+        percent_air: 0.2,
+        percent_naval: 0.05,
+        percent_orbital: 0.05,
+        personality_tags: ["Default", "SlowerExpansion"],
+        metal_drain_check: 0.24,
+        energy_drain_check: 0.35,
+        metal_demand_check: 0.41,
+        energy_demand_check: 0.5,
+        micro_type: 0,
+        go_for_the_kill: false,
+        priority_scout_metal_spots: false,
+        enable_commander_danger_responses: true,
+        neural_data_mod: 2.0,
+        adv_eco_mod: 1.0,
+        adv_eco_mod_alone: 1.0,
+        factory_build_delay_min: 1,
+        factory_build_delay_max: 3,
+        unable_to_expand_delay: 0,
+        per_expansion_delay: 0,
+        fabber_to_factory_ratio_basic: 1.0,
+        fabber_to_factory_ratio_advanced: 1.0,
+        fabber_alone_on_planet_mod: 2.0,
+        basic_to_advanced_factory_ratio: 0,
+        factory_alone_on_planet_mod: 0.5,
+        min_basic_fabbers: 2,
+        max_basic_fabbers: 6,
+        min_advanced_fabbers: 3,
+        max_advanced_fabbers: 20,
+        starting_location_evaluation_radius: 200,
       };
 
-      model.aiPersonalities(
-        addToObjectAtIndex(
-          model.aiPersonalities(),
-          "NormalPlus",
-          {
-            display_name: loc("!LOC:Normal") + "+",
-            percent_vehicle: 0.45,
-            percent_bot: 0.25,
-            percent_air: 0.2,
-            percent_naval: 0.05,
-            percent_orbital: 0.05,
-            personality_tags: ["Default", "SlowerExpansion"],
-            metal_drain_check: 0.24,
-            energy_drain_check: 0.35,
-            metal_demand_check: 0.41,
-            energy_demand_check: 0.5,
-            micro_type: 0,
-            go_for_the_kill: false,
-            priority_scout_metal_spots: false,
-            enable_commander_danger_responses: true,
-            neural_data_mod: 2.0,
-            adv_eco_mod: 1.0,
-            adv_eco_mod_alone: 1.0,
-            factory_build_delay_min: 1,
-            factory_build_delay_max: 3,
-            unable_to_expand_delay: 0,
-            per_expansion_delay: 0,
-            fabber_to_factory_ratio_basic: 1.0,
-            fabber_to_factory_ratio_advanced: 1.0,
-            fabber_alone_on_planet_mod: 2.0,
-            basic_to_advanced_factory_ratio: 0,
-            factory_alone_on_planet_mod: 0.5,
-            min_basic_fabbers: 2,
-            max_basic_fabbers: 6,
-            min_advanced_fabbers: 3,
-            max_advanced_fabbers: 20,
-            starting_location_evaluation_radius: 200,
-          },
-          3
-        )
-      );
+      for (var prop in model.aiPersonalities()) {
+        if (
+          Object.prototype.hasOwnProperty.call(model.aiPersonalities(), prop)
+        ) {
+          if (i === 3) {
+            temp["NormalPlus"] = newDifficulty;
+          }
+
+          temp[prop] = model.aiPersonalities()[prop];
+
+          i++;
+        }
+      }
+
+      model.aiPersonalities(temp);
     } catch (e) {
       console.error(e);
       console.error(JSON.stringify(e));
